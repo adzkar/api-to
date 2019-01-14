e7e619a23e8dc061d6d5ec9959dd0396e9629a6f
adzkar <adzkarfauzie02@gmail.com>
2019-01-14 22:26:32 +0700

    Update Images Table

---
+++ database/migrations/2019_01_14_152255_update_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->string('file_name')
+                  ->unique()
+                  ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            //
+        });
+    }
+}
