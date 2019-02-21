186e8e914d23efde78926f21aa5db9b002c87a03
adzkar <adzkarfauzie02@gmail.com>
2019-02-21 11:51:06 +0700

    Fix missing image url

--- app/Http/Resources/DetailResource.php
+++ app/Http/Resources/DetailResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 use App\Http\Resources\AnswerParResource as AnsRes;
+use App\Http\Resources\ImageResource as ImgRes;
 
 class DetailResource extends JsonResource
 {
@@ -19,6 +20,7 @@ class DetailResource extends JsonResource
         return [
           'id' => $this->id_dr,
           'question' => $this->question->content,
+          'image' => ImgRes::collection($this->question->images),
           'answer' => new AnsRes($this->answer),
           'options' => AnsRes::collection($this->question->answers),
         ];
